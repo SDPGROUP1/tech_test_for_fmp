462e3a357d7afd2dd26737b6d7ba822ccae32aa3
patrick <s1228955@sms.ed.ac.uk>
2017-08-30 13:42:08 +0100

    modified store to use localStorage

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import Note from './Note'
 
 const Notes = ({ notes }) => (
@@ -12,3 +13,9 @@ const Notes = ({ notes }) => (
         )}
     </div>
 )
+
+const mapStateToProps = (state) => ({
+    notes: state.notes
+})
+
+export default connect(mapStateToProps)(Notes)
